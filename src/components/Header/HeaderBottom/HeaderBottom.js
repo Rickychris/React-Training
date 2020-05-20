8211841a0b705fe0bb49b41f3f8859b087ea23da
Rickychris <55373853+Rickychris@users.noreply.github.com>
2020-05-20 12:39:23 +0530

    code improved

--- src/components/Header/HeaderBottom/HeaderBottom.js
+++ src/components/Header/HeaderBottom/HeaderBottom.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 
 
@@ -18,10 +18,10 @@ const headetBottomData = [
 const HeaderBottom = (props) => {
     // window.scrollTo(0, -1000);
     // const [tap, setTap] = useState(false);
-    const responsiveNav = () => {
-        props.ResponsiveNav();
-        // setTap(!tap)
-    }
+    // const responsiveNav = () => {
+    //     props.ResponsiveNav();
+    //     // setTap(!tap)
+    // }
     return (
         <div className={classes.Background}>
 
@@ -52,89 +52,3 @@ const mapDispatchToProps = dispatch => {
     };
 }
 export default connect(mapStateToProps, mapDispatchToProps)(HeaderBottom);
-
-// import React, { Component } from 'react';
-
-// // import { Link } from 'react-router-dom';
-// import { connect } from 'react-redux';
-// // import { Drawer } from 'antd';
-
-// import classes from './HeaderBottom.module.css';
-// import Logo from '../../../assets/images/vcep-logo.png';
-
-// const headetBottomData = [
-//     'ACCOUNT SUMMARY',
-//     'GRANTS',
-//     'CONTRIBUTIONS',
-//     'INVESTMENTS',
-//     'RESOURCES',
-//     'MY ACCOUNT',
-// ];
-// class Navbar extends Component {
-//     state = {
-//         visible: false
-//     }
-//     showDrawer = () => {
-//         console.log('open')
-//         this.setState({
-//             visible: true,
-//         });
-//     };
-//     onClose = () => {
-//         console.log('close')
-//         this.setState({
-//             visible: false,
-//         });
-//     };
-//     render() {
-//         return (
-//             <nav>
-//                 <div className={classes.Background}>
-
-//                     <div className={[classes.HeaderBottom, 'MaxWidth'].join(' ')}>
-//                         <img src={Logo} alt='logo' />
-//                         <ul className={classes.NavUL}>
-//                             {headetBottomData.map(data => <li key={data}>{data}</li>)}
-//                         </ul>
-//                         <div onClick={this.showDrawer}>
-//                             <div></div>
-//                             <div></div>
-//                             <div></div>
-//                         </div>
-//                     </div>
-//                 </div>
-//                 {/* <div className={classes.siteDrawerRenderInCurrentWrapper}>
-
-//                     <Drawer
-//                         className={classes.navDrawer}
-//                         title="Navigate To"
-//                         placement="right"
-//                         destroyOnClose
-//                         getContainer={false}
-//                         style={{ position: 'absolute' }}
-//                         // closable={false}
-//                         onClose={this.onClose}
-//                         visible={this.state.visible}
-//                     >
-//                         <ul className={classes.NavULRes}>
-//                             {headetBottomData.map(data => <li key={data}>{data}</li>)}
-//                             <li><p onClick={this.props.drawerHandler}>Need Assistance?</p></li>
-//                         </ul>
-//                     </Drawer>
-//                 </div> */}
-
-//             </nav >
-//         );
-//     }
-// }
-// const mapStateToProps = state => {
-//     return {
-//         // showNav: state.ResponsiveNav,
-//     };
-// }
-// const mapDispatchToProps = dispatch => {
-//     return {
-//         ResponsiveNav: () => dispatch({ type: 'ResponsiveNav' }),
-//     };
-// }
-// export default connect(mapStateToProps, mapDispatchToProps)(Navbar);
--- src/components/Header/HeaderTop/select.js
+++ src/components/Header/HeaderTop/select.js
@@ -6,7 +6,6 @@ class Select extends React.Component {
     render() {
         return (
             <select onChange={(e) => this.props.selectionChanged(e.target.value)}>
-                {/* {console.log(this.props.allFunds[1].name)} */}
                 {this.props.allFunds.map(data => <option key={data.id} value={data.id}>{data.name}</option>)}
             </select>
         );
